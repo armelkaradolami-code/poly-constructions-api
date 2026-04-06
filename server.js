b9f01b1b09b53d1c87c4886bcd4d61ba59c9dcdb
armelkaradolami-code <armel.karadolami@gmail.com>
2026-04-06 23:04:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+import express from 'express';
+import { Resend } from 'resend';
 
+const app = express();
+app.use(express.json());
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.post('/api/send-email', async (req, res) => {
+  const { to, subject, message } = req.body;
+
+  await resend.emails.send({
+    from: 'contact@ton-domaine.ch',
+    to,
+    subject,
+    html: message
+  });
+
+  res.json({ success: true });
+});
+
+app.listen(3000);
