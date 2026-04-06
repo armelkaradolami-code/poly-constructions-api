d2e14860f449c27da0d6a2d7e7971b51be188044
armelkaradolami-code <armel.karadolami@gmail.com>
2026-04-06 23:21:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,7 @@ app.post('/api/send-email', async (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
